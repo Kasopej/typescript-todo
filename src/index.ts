6b7f78a622aaa311bbfd63323f34074880303c27
Kasope Johnson <kasopej@gmail.com>
2022-10-08 00:29:26 +0100

    adds event listener to form element

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.querySelector<HTMLFormElement>("#new-task-form");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+form?.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    
+    if(input && (input.value === "" || input.value === null)) {}
+    return;
+})
