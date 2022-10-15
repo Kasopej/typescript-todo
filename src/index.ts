3cd413795b872525aeda68c4a3326f1bb00076e6
Kasope Johnson <kasopej@gmail.com>
2022-10-15 04:37:12 +0100

    imports bootstrap modules. Demo build successful

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { v4 as uuidV4 } from 'uuid';
+import "bootstrap/scss/bootstrap.scss"
+import "bootstrap"
 
 interface Task {
   id: string;
@@ -13,59 +15,59 @@ const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.querySelector<HTMLFormElement>('#new-task-form');
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
-const tasks: Tasks = loadTasks();
-tasks.forEach((task) => {
-  addListItem(task);
-});
+// const tasks: Tasks = loadTasks();
+// tasks.forEach((task) => {
+//   addListItem(task);
+// });
 
-form?.addEventListener('submit', (evt) => {
-  evt.preventDefault();
+// form?.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
 
-  if (input) {
-    if (!input.value) return;
+//   if (input) {
+//     if (!input.value) return;
 
-    const newTask: Task = {
-      id: uuidV4(),
-      title: input.value,
-      completed: false,
-      createdAt: new Date(),
-    };
+//     const newTask: Task = {
+//       id: uuidV4(),
+//       title: input.value,
+//       completed: false,
+//       createdAt: new Date(),
+//     };
 
-    addListItem(newTask);
-    input.value = '';
+//     addListItem(newTask);
+//     input.value = '';
 
-    saveTasks(newTask);
-  }
-});
+//     saveTasks(newTask);
+//   }
+// });
 
-function addListItem<T>(newTask: Task) {
-  const listItem = document.createElement('li');
-  const label = document.createElement('label');
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.checked = newTask.completed;
+// function addListItem<T>(newTask: Task) {
+//   const listItem = document.createElement('li');
+//   const label = document.createElement('label');
+//   const checkbox = document.createElement('input');
+//   checkbox.type = 'checkbox';
+//   checkbox.checked = newTask.completed;
 
-  label.append(checkbox, newTask.title);
-  listItem.append(label);
-  list && list.append(listItem);
+//   label.append(checkbox, newTask.title);
+//   listItem.append(label);
+//   list && list.append(listItem);
 
-  checkbox.addEventListener('change', function (evt) {
-    const target = evt.currentTarget as HTMLInputElement;
-    newTask.completed = target.checked;
-    saveTasks(newTask);
-  });
-}
+//   checkbox.addEventListener('change', function (evt) {
+//     const target = evt.currentTarget as HTMLInputElement;
+//     newTask.completed = target.checked;
+//     saveTasks(newTask);
+//   });
+// }
 
-function saveTasks(task: Task) {
-  const foundTaskIndex: number = tasks.findIndex(
-    (savedTask) => savedTask.id === task.id,
-  );
-  foundTaskIndex >= 0
-    ? tasks.splice(foundTaskIndex, 1, task)
-    : tasks.push(task);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+// function saveTasks(task: Task) {
+//   const foundTaskIndex: number = tasks.findIndex(
+//     (savedTask) => savedTask.id === task.id,
+//   );
+//   foundTaskIndex >= 0
+//     ? tasks.splice(foundTaskIndex, 1, task)
+//     : tasks.push(task);
+//   localStorage.setItem('tasks', JSON.stringify(tasks));
+// }
 
-function loadTasks(): Tasks {
-  return JSON.parse(localStorage.getItem('tasks') as string) || [];
-}
+// function loadTasks(): Tasks {
+//   return JSON.parse(localStorage.getItem('tasks') as string) || [];
+// }
