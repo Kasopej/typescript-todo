dc1f57ff043dfe0ed00746473e5530586a5e99fa
Kasope Johnson <kasopej@gmail.com>
2022-10-18 02:21:39 +0100

    another test dbl quote

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ interface Task {
 type Tasks = Array<Task>;
 
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.querySelector<HTMLFormElement>('#new-task-form');
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
 // const tasks: Tasks = loadTasks();
