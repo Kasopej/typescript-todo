90d45ee8c4e15a06d19880ec0c699a7822513c98
Kasope Johnson <kasopej@gmail.com>
2022-11-29 15:49:30 +0100

    generates  appends templates for empty or available todos

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,32 @@
 import './assets/styles/scss/custom.scss';
 import 'bootstrap';
 
-interface Task {
+interface Todo {
   id: string;
-  title: string;
+  description: string;
   completed: boolean;
   createdAt: Date;
 }
+type Todos = Array<Todo>;
 
-type HTMLString = string;
-type Tasks = Array<Task>;
+//retrieve Todos from store
+const todosInStore = JSON.parse(localStorage.getItem('todos'));
+const todos: Todos = todosInStore ? todosInStore : [];
 
 const allTodosPane = document.getElementById('pills-home');
-const input = document.querySelector<HTMLInputElement>('#new-task-title');
-
 const todoTemplate = document.getElementById(
   'todo-template',
 ) as HTMLTemplateElement;
+const emptyTodosTemplate = document.getElementById(
+  'no-todos-template',
+) as HTMLTemplateElement;
 
-const todoTemplateClone = todoTemplate.content.cloneNode(true);
-allTodosPane.appendChild(todoTemplateClone);
-// const mainHTMLElement: HTMLElement = document.querySelector('main');
-// mainHTMLElement.innerHTML = noTodosHTML;
+if (!todos.length) {
+  const emptyTemplateClone = emptyTodosTemplate.content.cloneNode(true);
+  allTodosPane.appendChild(emptyTemplateClone);
+} else {
+  todos.forEach((todo) => {
+    const todoTemplateClone = todoTemplate.content.cloneNode(true);
+    allTodosPane.appendChild(todoTemplateClone);
+  });
+}
