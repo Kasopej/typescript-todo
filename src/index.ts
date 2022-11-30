9351df39050c1cb491450d706b28e463ae6554b6
Kasope Johnson <kasopej@gmail.com>
2022-11-30 01:27:15 +0100

    adds functions for creating and appending todos

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,94 @@ import './assets/styles/scss/custom.scss';
 import 'bootstrap';
 
 interface Todo {
-  id: string;
+  id: number;
   description: string;
   completed: boolean;
   createdAt: Date;
 }
 type Todos = Array<Todo>;
+type toggle = 'show' | 'hide';
 
+const allTodosPane = document.getElementById('pills-all-todos');
+const pendingTodosPane = document.getElementById('pills-pending-todos');
+const createTodoModal = document.getElementById('newTodoModal');
+const createTodoInput = createTodoModal.getElementsByTagName('input')[0];
+const createTodoButton = document.getElementById(
+  'new-todo-btn',
+) as HTMLButtonElement;
+const todoTemplate = document.getElementById(
+  'todo-template',
+) as HTMLTemplateElement;
+const emptyTodoElements = Array.from(
+  document.getElementsByClassName('empty-todos'),
+);
+
+function createTodo(todoText: string, todos: Todos): Todo {
+  let nextId = 1;
+
+  todos.forEach((todo) => {
+    nextId = todo.id > nextId ? todo.id + 1 : nextId;
+  });
+
+  const newTodo =
+    todos[
+      todos.push({
+        id: nextId,
+        description: todoText,
+        completed: false,
+        createdAt: new Date(),
+      }) - 1
+    ];
+  localStorage.setItem('todos', JSON.stringify(todos));
+  return newTodo;
+}
+
+function appendTodoToDom(todo: Todo) {
+  const todoTemplateClone = todoTemplate.content.firstElementChild?.cloneNode(
+    true,
+  ) as HTMLElement;
+
+  const todoTemplateInput = todoTemplateClone.getElementsByTagName(
+    'input',
+  )[0] as HTMLInputElement;
+  todoTemplateInput.value = todo.description;
+  todoTemplateInput.addEventListener('click', function () {
+    this.readOnly = false;
+  });
+  //TODO: edit todo via input element. Create function for this
+
+  todoTemplateClone.getElementsByClassName('todo-timestamp')[0].textContent =
+    new Date(todo.createdAt).toTimeString();
+  const todoTemplateStatusBtn =
+    todoTemplateClone.getElementsByClassName('todo-status-btn')[0];
+
+  allTodosPane.appendChild(todoTemplateClone);
+  if (todo.completed)
+    todoTemplateStatusBtn.classList.add('bi-check-circle-fill');
+  else todoTemplateStatusBtn.classList.add('bi-check-circle');
+}
+
+function toggleEmptyTodoElements(toggle: toggle) {
+  emptyTodoElements.forEach((emptyTodoEl) => {
+    if (toggle === 'hide') emptyTodoEl.classList.add('d-none');
+    else emptyTodoEl.classList.remove('d-none');
+  });
+}
+
+//
 //retrieve Todos from store
 const todosInStore = JSON.parse(localStorage.getItem('todos'));
 const todos: Todos = todosInStore ? todosInStore : [];
 
-const allTodosPane = document.getElementById('pills-all-todos');
-const pendingTodosPane = document.getElementById('pills-pending-todos');
-const todoTemplate = document.getElementById(
-  'todo-template',
-) as HTMLTemplateElement;
-
-const emptyTodos = Array.from(document.getElementsByClassName('empty-todos'));
+createTodoButton.addEventListener('click', () => {
+  if (!createTodoInput.value) return;
+  appendTodoToDom(createTodo(createTodoInput.value, todos));
+});
 
 if (todos.length) {
-  emptyTodos.forEach((emptyTodoEl) => emptyTodoEl.classList.add('d-none'));
+  toggleEmptyTodoElements('hide');
 
   todos.forEach((todo) => {
-    const todoTemplateClone =
-      todoTemplate.content.firstElementChild.cloneNode(true);
-    allTodosPane.appendChild(todoTemplateClone);
+    appendTodoToDom(todo);
   });
 }
